263017e0da08d3644a3262e714c28fd8f789e709
Pham Duc Trung <84474382+tphamduc@users.noreply.github.com>
2022-12-09 14:52:56 +0700

    Add files via upload

--- Word.py
+++ Word.py
@@ -1,6 +1,9 @@
 from docx import Document
 from docx.shared import Pt
 from docx.text.run import Font
+from docx.enum.style import WD_STYLE_TYPE
+from docx.text.run import Run
+from docx.text.font import ColorFormat
 
 def ReadText(filename):
     document = Document(filename)
@@ -27,30 +30,41 @@ def FontSize(filename):
     for c in all_12pt_text_set:
         print(c)
 
-# FontSize("test.docx")
-
-# document = Document("test.docx")
-# for p in document.paragraphs:
-#     for run in p.runs:
-#         print(run.font.size)
-
-def FontBold(filename):
-    document = Document(filename)
-    all_bold = []
+def Font_Bold(Filename):
+    document = Document(Filename)
+    all_text_bold = []
     for p in document.paragraphs:
         for run in p.runs:
-            if run.font.bold == Font.cs_bold:
-                all_bold.append(p.text)
-                print(p)
+            if run.font.bold:
+                all_text_bold.append(p.text)
 
-    all_bold_set = []
-    # all_bold_set = set(all_bold)
-    #
-    # for c in all_bold_set:
-    #     print(c)
+    text_bold_set = []
+    text_bold_set = set(all_text_bold)
+    for c in text_bold_set:
+        print(c)
 
+# Font_Bold("word.docx")
 
-FontBold("test.docx")
+def Font_Italic(Filename):
+    document = Document(Filename)
+    all_text_italic = []
+    for p in document.paragraphs:
+        for run in p.runs:
+            if run.font.italic:
+                all_text_italic.append(p.text)
+
+    text_italic_set = []
+    text_italic_set = set(all_text_italic)
+    for c in text_italic_set:
+        print(c)
+
+# Font_Italic("word.docx")
+
+document = Document("word.docx")
+all_text_color = []
+for p in document.paragraphs:
+    for run in p.runs:
+        print(run.font.color.rgb)
 
 
 
